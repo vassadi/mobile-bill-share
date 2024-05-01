a3966c8294445a617a2cb98a4a9cba9285c9e223
Srinivasarao Vassadi <srinivas.vassadi@gmail.com>
2024-05-01 00:28:21 -0400

    fix: vite config updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mobile-bill-share/',
   build: {
     rollupOptions: {
       external: ['firebase-admin'],
